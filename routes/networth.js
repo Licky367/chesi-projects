24cbf47347ccd31edd6f49e75fdf1e932fd2468c
Licky367 <licky367@gmail.com>
2026-08-08 19:26:23 +0300

    Update networth.js

--- routes/networth.js
+++ routes/networth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const networkController = require("../controllers/networkController");
+const networkController = require("../controllers/networthController");
 
 
 /* ==========================================================
