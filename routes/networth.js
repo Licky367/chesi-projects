a1925f8d2cdc37e792eba34decdc193beb3bbcbe
Licky367 <licky367@gmail.com>
2026-08-09 13:33:27 +0300

    Update networth.js

--- routes/networth.js
+++ routes/networth.js
@@ -2,58 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-
 const updateAssetController =
     require("../controllers/networth/updateAsset");
 
 
 /* ==========================================================
-   MAIN NET WORTH
-========================================================== */
-
-router.get(
-    "/",
-    networthController.getNetWorth
-);
-
-
-/* ==========================================================
-   DAIRY FARM
-========================================================== */
-
-router.get(
-    "/structure/:id",
-    networthController.getDairyFarm
-);
-
-
-/* ==========================================================
-   ADD ASSET TO DAIRY FARM
-========================================================== */
-
-router.get(
-    "/structure/:id/add",
-    networthController.getAddAsset
-);
-
-router.post(
-    "/structure/:id/add",
-    networthController.addAsset
-);
-
-
-/* ==========================================================
-   ASSET DETAILS / EDIT PAGE
-========================================================== */
-
-router.get(
-    "/asset/:id",
-    networthController.getAsset
-);
-
-
-/* ==========================================================
    UPDATE ASSET
+
+   Mounted as:
+       /networth
+
+   Final endpoint:
+       POST /networth/asset/:id
 ========================================================== */
 
 router.post(
