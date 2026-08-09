758e7a9d91d237d8fc224723a06345cd4598c4df
Licky367 <licky367@gmail.com>
2026-08-09 16:22:50 +0300

    Create add.js

---
+++ routes/add.js
@@ -0,0 +1,42 @@
+const express =
+    require("express");
+
+const router =
+    express.Router();
+
+
+const addController =
+    require("../controllers/addController");
+
+
+const upload =
+    require("../middleware/uploadMidleware");
+
+
+// ==========================================================
+// GET /add
+// ==========================================================
+
+router.get(
+    "/",
+    addController.getAddPage
+);
+
+
+// ==========================================================
+// POST /add
+//
+// Handles:
+// - profileImage
+// - all normal form fields
+// ==========================================================
+
+router.post(
+    "/",
+    upload.single("profileImage"),
+    addController.createDairy
+);
+
+
+module.exports =
+    router;
