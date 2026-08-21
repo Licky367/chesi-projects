1a641063a282728fbb16cd02c3069dc43df204de
Licky367 <licky367@gmail.com>
2026-08-21 15:00:37 +0300

    Update index.js

--- controllers/storage/index.js
+++ controllers/storage/index.js
@@ -1,331 +1,105 @@
 // =========================================================
 // controllers/storage/index.js
 // STORAGE CONTROLLER ENTRY POINT
-// ==========================================================
-//
-// Combines:
-//
-//     list.js
-//     add.js
-//     contents.js
-//
-// IMPORTANT:
-//
-// Every property exported here MUST be a function,
-// because routes/storage.js passes these directly to Express.
-//
-// ==========================================================
+// =========================================================
+
+const listController = require("./list");
+const addController = require("./add");
+const addNewController = require("./addNew");
+const contentsController = require("./contents");
 
 
-const listController =
-    require("./list");
+function resolveHandler(controller, names, controllerName) {
 
-
-const addController =
-    require("./add");
-
-
-const contentsController =
-    require("./contents");
-
-
-// ==========================================================
-// CONTROLLER HANDLER RESOLVER
-// ==========================================================
-//
-// Allows a controller module to export either:
-//
-//     module.exports = function (...) {}
-//
-// OR:
-//
-//     module.exports = {
-//         functionName: function (...) {}
-//     }
-//
-// ==========================================================
-
-function resolveHandler(
-    controller,
-    names,
-    controllerName
-) {
-
-    // ------------------------------------------------------
-    // DIRECT FUNCTION EXPORT
-    // ------------------------------------------------------
-
-    if (
-        typeof controller === "function"
-    ) {
-
+    if (typeof controller === "function") {
         return controller;
-
     }
 
-
-    // ------------------------------------------------------
-    // NAMED FUNCTION EXPORT
-    // ------------------------------------------------------
-
-    if (
-        controller &&
-        typeof controller === "object"
-    ) {
-
-        for (
-            const name of names
-        ) {
-
-            if (
-                typeof controller[name] ===
-                "function"
-            ) {
-
+    if (controller && typeof controller === "object") {
+        for (const name of names) {
+            if (typeof controller[name] === "function") {
                 return controller[name];
-
             }
-
         }
-
     }
-
-
-    // ------------------------------------------------------
-    // INVALID CONTROLLER
-    // ------------------------------------------------------
 
     throw new TypeError(
         `Storage controller "${controllerName}" does not export a valid route handler function.`
     );
-
 }
 
 
-// ==========================================================
-// LIST
-// ==========================================================
+const list = resolveHandler(listController, ["list", "index"], "list");
 
-const list =
-    resolveHandler(
+const form = resolveHandler(
+    addController,
+    ["getAddStorage", "form", "showForm", "addForm"],
+    "form"
+);
 
-        listController,
-
-        [
-            "list",
-            "index"
-        ],
-
-        "list"
-
-    );
-
+const create = resolveHandler(
+    addController,
+    ["createStorage", "create", "add"],
+    "create"
+);
 
 // ==========================================================
-// ADD FORM
-// ==========================================================
-//
-// add.js exports:
-//
-//     getAddStorage
-//
-// This is exposed to the routes as:
-//
-//     form
-//
+// ADD ITEM DIRECTLY TO EXISTING STORAGE
 // ==========================================================
 
-const form =
-    resolveHandler(
+const addNewForm = resolveHandler(
+    addNewController,
+    ["getAddNewStorage", "form", "showForm", "addNewForm"],
+    "addNewForm"
+);
 
-        addController,
-
-        [
-            "getAddStorage",
-            "form",
-            "showForm",
-            "addForm"
-        ],
-
-        "form"
-
-    );
+const addNewItem = resolveHandler(
+    addNewController,
+    ["addNewItem", "create", "add"],
+    "addNewItem"
+);
 
 
-// ==========================================================
-// CREATE STORAGE
-// ==========================================================
-//
-// add.js exports:
-//
-//     createStorage
-//
-// This is exposed to the routes as:
-//
-//     create
-//
-// ==========================================================
+const contents = resolveHandler(
+    contentsController,
+    ["contents", "index"],
+    "contents"
+);
 
-const create =
-    resolveHandler(
+const addItems = resolveHandler(
+    contentsController,
+    ["addItems"],
+    "addItems"
+);
 
-        addController,
+const omitItems = resolveHandler(
+    contentsController,
+    ["omitItems"],
+    "omitItems"
+);
 
-        [
-            "createStorage",
-            "create",
-            "add"
-        ],
+const reshuffleItems = resolveHandler(
+    contentsController,
+    ["reshuffleItems"],
+    "reshuffleItems"
+);
 
-        "create"
+const updateQuantity = resolveHandler(
+    contentsController,
+    ["updateQuantity", "updateFeedQuantity"],
+    "updateQuantity"
+);
 
-    );
-
-
-// ==========================================================
-// STORAGE CONTENTS
-// ==========================================================
-
-const contents =
-    resolveHandler(
-
-        contentsController,
-
-        [
-            "contents",
-            "index"
-        ],
-
-        "contents"
-
-    );
-
-
-// ==========================================================
-// ADD ITEMS
-// ==========================================================
-
-const addItems =
-    resolveHandler(
-
-        contentsController,
-
-        [
-            "addItems"
-        ],
-
-        "addItems"
-
-    );
-
-
-// ==========================================================
-// OMIT ITEMS
-// ==========================================================
-//
-// IMPORTANT:
-//
-// This remains available for NORMAL storage.
-//
-// The controller/service will reject manual omission from
-// AgroStore.
-//
-// ==========================================================
-
-const omitItems =
-    resolveHandler(
-
-        contentsController,
-
-        [
-            "omitItems"
-        ],
-
-        "omitItems"
-
-    );
-
-
-// ==========================================================
-// RESHUFFLE ITEMS
-// ==========================================================
-//
-// IMPORTANT:
-//
-// This remains available for NORMAL storage.
-//
-// The controller/service will reject reshuffling involving
-// AgroStore.
-//
-// ==========================================================
-
-const reshuffleItems =
-    resolveHandler(
-
-        contentsController,
-
-        [
-            "reshuffleItems"
-        ],
-
-        "reshuffleItems"
-
-    );
-
-
-// ==========================================================
-// UPDATE QUANTITY
-// ==========================================================
-//
-// AgroStore operation.
-//
-// The service is responsible for:
-//
-//     quantity > 0
-//         -> keep item in AgroStore
-//
-//     quantity === 0
-//         -> automatically omit item
-//
-// Negative quantities are rejected by the service.
-//
-// ==========================================================
-
-const updateQuantity =
-    resolveHandler(
-
-        contentsController,
-
-        [
-            "updateQuantity",
-            "updateFeedQuantity"
-        ],
-
-        "updateQuantity"
-
-    );
-
-
-// ==========================================================
-// EXPORT
-// ==========================================================
 
 module.exports = {
-
     list,
-
     form,
-
     create,
-
+    addNewForm,
+    addNewItem,
     contents,
-
     addItems,
-
     omitItems,
-
     reshuffleItems,
-
     updateQuantity
-
 };
