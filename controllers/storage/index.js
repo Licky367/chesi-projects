ada15e9a3b8c0f9e189bcd8de5dba867e235913a
Licky367 <licky367@gmail.com>
2026-08-22 13:00:56 +0300

    Update index.js

--- controllers/storage/index.js
+++ controllers/storage/index.js
@@ -9,97 +9,246 @@ const addNewController = require("./addNew");
 const contentsController = require("./contents");
 
 
-function resolveHandler(controller, names, controllerName) {
+// =========================================================
+// RESOLVE CONTROLLER HANDLER
+// =========================================================
+
+function resolveHandler(
+    controller,
+    names,
+    controllerName
+) {
 
     if (typeof controller === "function") {
         return controller;
     }
 
-    if (controller && typeof controller === "object") {
-        for (const name of names) {
-            if (typeof controller[name] === "function") {
+    if (
+        controller &&
+        typeof controller === "object"
+    ) {
+
+        for (
+            const name
+            of names
+        ) {
+
+            if (
+                typeof controller[name] === "function"
+            ) {
+
                 return controller[name];
+
             }
+
         }
+
     }
 
     throw new TypeError(
         `Storage controller "${controllerName}" does not export a valid route handler function.`
     );
+
 }
 
 
-const list = resolveHandler(listController, ["list", "index"], "list");
+// =========================================================
+// STORAGE LIST
+// =========================================================
 
-const form = resolveHandler(
-    addController,
-    ["getAddStorage", "form", "showForm", "addForm"],
-    "form"
-);
+const list =
+    resolveHandler(
+        listController,
+        [
+            "list",
+            "index"
+        ],
+        "list"
+    );
 
-const create = resolveHandler(
-    addController,
-    ["createStorage", "create", "add"],
-    "create"
-);
 
-// ==========================================================
+// =========================================================
+// ADD STORAGE
+// =========================================================
+
+const form =
+    resolveHandler(
+        addController,
+        [
+            "getAddStorage",
+            "form",
+            "showForm",
+            "addForm"
+        ],
+        "form"
+    );
+
+
+const create =
+    resolveHandler(
+        addController,
+        [
+            "createStorage",
+            "create",
+            "add"
+        ],
+        "create"
+    );
+
+
+// =========================================================
 // ADD ITEM DIRECTLY TO EXISTING STORAGE
-// ==========================================================
+// =========================================================
 
-const addNewForm = resolveHandler(
-    addNewController,
-    ["getAddNewStorage", "form", "showForm", "addNewForm"],
-    "addNewForm"
-);
+const addNewForm =
+    resolveHandler(
+        addNewController,
+        [
+            "getAddNewStorage",
+            "form",
+            "showForm",
+            "addNewForm"
+        ],
+        "addNewForm"
+    );
 
-const addNewItem = resolveHandler(
-    addNewController,
-    ["addNewItem", "create", "add"],
-    "addNewItem"
-);
+
+const addNewItem =
+    resolveHandler(
+        addNewController,
+        [
+            "addNewItem",
+            "create",
+            "add"
+        ],
+        "addNewItem"
+    );
 
 
-const contents = resolveHandler(
-    contentsController,
-    ["contents", "index"],
-    "contents"
-);
+// =========================================================
+// STORAGE CONTENTS
+// =========================================================
 
-const addItems = resolveHandler(
-    contentsController,
-    ["addItems"],
-    "addItems"
-);
+const contents =
+    resolveHandler(
+        contentsController,
+        [
+            "contents",
+            "index"
+        ],
+        "contents"
+    );
 
-const omitItems = resolveHandler(
-    contentsController,
-    ["omitItems"],
-    "omitItems"
-);
 
-const reshuffleItems = resolveHandler(
-    contentsController,
-    ["reshuffleItems"],
-    "reshuffleItems"
-);
+// =========================================================
+// STORAGE CONTENT ITEM DETAILS
+// =========================================================
+//
+// GET:
+//
+// /storage/:dairyId/contents/:storageId/details/:itemId
+//
+// This resolves the handler responsible for rendering:
+//
+//     views/storage/content-item.ejs
+//
+// =========================================================
 
-const updateQuantity = resolveHandler(
-    contentsController,
-    ["updateQuantity", "updateFeedQuantity"],
-    "updateQuantity"
-);
+const contentItem =
+    resolveHandler(
+        contentsController,
+        [
+            "contentItem",
+            "itemDetails",
+            "details"
+        ],
+        "contentItem"
+    );
 
+
+// =========================================================
+// ADD ITEMS TO STORAGE
+// =========================================================
+
+const addItems =
+    resolveHandler(
+        contentsController,
+        [
+            "addItems"
+        ],
+        "addItems"
+    );
+
+
+// =========================================================
+// OMIT ITEMS FROM STORAGE
+// =========================================================
+
+const omitItems =
+    resolveHandler(
+        contentsController,
+        [
+            "omitItems"
+        ],
+        "omitItems"
+    );
+
+
+// =========================================================
+// RESHUFFLE ITEMS
+// =========================================================
+
+const reshuffleItems =
+    resolveHandler(
+        contentsController,
+        [
+            "reshuffleItems"
+        ],
+        "reshuffleItems"
+    );
+
+
+// =========================================================
+// UPDATE STORAGE QUANTITY
+// =========================================================
+
+const updateQuantity =
+    resolveHandler(
+        contentsController,
+        [
+            "updateQuantity",
+            "updateFeedQuantity"
+        ],
+        "updateQuantity"
+    );
+
+
+// =========================================================
+// EXPORT
+// =========================================================
 
 module.exports = {
+
     list,
+
     form,
+
     create,
+
     addNewForm,
+
     addNewItem,
+
     contents,
+
+    contentItem,
+
     addItems,
+
     omitItems,
+
     reshuffleItems,
+
     updateQuantity
+
 };
