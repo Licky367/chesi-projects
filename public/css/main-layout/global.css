f0786d5ccc021f3cab48d6bb11b8ed312e5117f2
Licky367 <licky367@gmail.com>
2026-08-27 12:39:24 +0300

    Create global.css

---
+++ public/css/main-layout/global.css
@@ -0,0 +1,16 @@
+/* =========================================================
+   GLOBAL
+========================================================= */
+
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:Arial,sans-serif;
+}
+
+html,
+body{
+    min-height:100%;
+    background:#f4f6f8;
+}
