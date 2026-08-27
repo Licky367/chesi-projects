ca5d80b8a61b657dde9736e1ba6fde14bf347204
Licky367 <licky367@gmail.com>
2026-08-28 00:34:25 +0300

    Update products.js

--- routes/corevester/products.js
+++ routes/corevester/products.js
@@ -11,4 +11,6 @@ router.post("/add-to-cart", productsController.addToCart);
 router.post("/remove-from-cart", productsController.removeFromCart);
 router.post("/checkout", productsController.checkout);
 
+router.get("/:id", productsController.productDetails);
+
 module.exports = router;
