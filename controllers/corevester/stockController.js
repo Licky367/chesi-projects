2d978bffb3a04b1ff4bf8c0d0f4455fda3063cf4
Licky367 <licky367@gmail.com>
2026-08-27 22:50:58 +0300

    Create stockController.js

---
+++ controllers/corevester/stockController.js
@@ -0,0 +1,45 @@
+const stockService = require("../../services/corevester/stockService");
+
+exports.stockPage = async (req, res) => {
+    try{
+        const [stockList, stats] = await Promise.all([
+            stockService.getAllStock(),
+            stockService.getStats()
+        ]);
+        res.render("corevester/admin/stock-entry", {
+            title: "Stock Warehouse - COREVESTER",
+            stockList,
+            stats,
+            currentPath: req.path
+        });
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+};
+
+exports.createStock = async (req, res) => {
+    try{
+        await stockService.createOrUpdateStock(req.body);
+        res.json({ success: true });
+    }catch(err){
+        res.status(400).json({ success: false, message: err.message });
+    }
+};
+
+exports.updateStock = async (req, res) => {
+    try{
+        await stockService.updateStock(req.params.id, req.body);
+        res.json({ success: true });
+    }catch(err){
+        res.status(400).json({ success: false, message: err.message });
+    }
+};
+
+exports.deleteStock = async (req, res) => {
+    try{
+        await stockService.deleteStock(req.params.id);
+        res.json({ success: true });
+    }catch(err){
+        res.status(400).json({ success: false, message: err.message });
+    }
+};
