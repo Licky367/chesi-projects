669ee669b29e55a165d1c206f0b37f98575e55c4
Licky367 <licky367@gmail.com>
2026-08-27 20:40:00 +0300

    Create products.js

---
+++ models/corevester/products.js
@@ -0,0 +1,45 @@
+// ==========================================================
+// models/corevester/products.js
+// PRODUCT MODEL
+// ==========================================================
+
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        category: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        image: {
+            type: String,
+            trim: true,
+            default: ""
+        },
+
+        units: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+
+        unitSellPrice: {
+            type: Number,
+            required: true,
+            min: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Product", productSchema);
