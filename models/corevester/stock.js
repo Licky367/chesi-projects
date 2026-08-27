16b7e716804f51b93a226698c00b90c92eb85313
Licky367 <licky367@gmail.com>
2026-08-27 20:25:37 +0300

    Create stock.js

---
+++ models/corevester/stock.js
@@ -0,0 +1,33 @@
+// ==========================================================
+// models/stock.js
+// STOCK MODEL
+// ==========================================================
+
+const mongoose = require("mongoose");
+
+const stockSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        units: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+
+        unitBuyPrice: {
+            type: Number,
+            required: true,
+            min: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Stock", stockSchema);
