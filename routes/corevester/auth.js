b53366454c17a806e8b0e7e51bf9fc606b6bedc5
Licky367 <licky367@gmail.com>
2026-08-28 22:24:28 +0300

    Update auth.js

--- routes/corevester/auth.js
+++ routes/corevester/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const authController = require("../../controllers/corevester/auth");
 
-// ================= ROUTES =================
+// ================= ROUTES ================
 
 // SIGNUP
 router.get("/signup", authController.renderSignup);
