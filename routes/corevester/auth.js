4de34c3823dbb33b08e407d0de8520991e6cf0c5
Licky367 <licky367@gmail.com>
2026-08-28 21:41:51 +0300

    Update auth.js

--- routes/corevester/auth.js
+++ routes/corevester/auth.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../../controllers/corevester/auth");
 
-router.get("/signup", authController.showSignup);
+const authController = require("../controllers/authController");
+
+// ================= ROUTES =================
+
+// SIGNUP
+router.get("/signup", authController.renderSignup);
 router.post("/signup", authController.signup);
-router.get("/login", authController.showLogin);
+
+// LOGIN
+router.get("/login", authController.renderLogin);
 router.post("/login", authController.login);
+
+// LOGOUT
 router.get("/logout", authController.logout);
+
+// FORGOT PASSWORD
+router.get("/forgot-password", authController.renderForgot);
+router.post("/forgot-password", authController.forgotPassword);
+
+// RESET PASSWORD
+router.get("/reset-password/:token", authController.renderReset);
+router.post("/reset-password/:token", authController.resetPassword);
 
 module.exports = router;
