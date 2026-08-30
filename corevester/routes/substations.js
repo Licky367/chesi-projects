ceae070fac2f198b1ac656a9bc51dd3c0b4f9af7
Licky367 <licky367@gmail.com>
2026-08-30 05:32:41 +0300

    Update substations.js

--- corevester/routes/substations.js
+++ corevester/routes/substations.js
@@ -1,1 +1,39 @@
-const router=require('express').Router(),c=require('../controllers/substations');router.get('/',c.list);router.get('/new',c.newForm);router.post('/',c.create);router.get('/product/:id',c.productDetail);router.get('/:id',c.detail);module.exports=router;
+const router = require("express").Router();
+
+const c =
+    require("../controllers/substations");
+
+const requireAdmin =
+    require("../middleware/requireAdmin");
+
+router.get(
+    "/",
+    requireAdmin,
+    c.list
+);
+
+router.get(
+    "/new",
+    requireAdmin,
+    c.newForm
+);
+
+router.post(
+    "/",
+    requireAdmin,
+    c.create
+);
+
+router.get(
+    "/product/:id",
+    requireAdmin,
+    c.productDetail
+);
+
+router.get(
+    "/:id",
+    requireAdmin,
+    c.detail
+);
+
+module.exports = router;
