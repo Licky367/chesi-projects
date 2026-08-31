0594ee05063a232579fa8e40275069fe385bae30
Licky367 <licky367@gmail.com>
2026-08-31 15:14:18 +0300

    Ruts

--- corevester/routes/substations.js
+++ corevester/routes/substations.js
@@ -1,39 +1,12 @@
 const router = require("express").Router();
+const c = require("../controllers/substations");
+const requireAdmin = require("../middleware/requireAdmin");
 
-const c =
-    require("../controllers/substations");
-
-const requireAdmin =
-    require("../middleware/requireAdmin");
-
-router.get(
-    "/",
-    requireAdmin,
-    c.list
-);
-
-router.get(
-    "/new",
-    requireAdmin,
-    c.newForm
-);
-
-router.post(
-    "/",
-    requireAdmin,
-    c.create
-);
-
-router.get(
-    "/product/:id",
-    requireAdmin,
-    c.productDetail
-);
-
-router.get(
-    "/:id",
-    requireAdmin,
-    c.detail
-);
+router.get("/", requireAdmin, c.list);
+router.get("/new", requireAdmin, c.newForm);
+router.post("/", requireAdmin, c.create);
+router.get("/product/:id", requireAdmin, c.productDetail);
+router.post("/product/:id", requireAdmin, c.updateProductUnits);
+router.get("/:id", requireAdmin, c.detail);
 
 module.exports = router;
