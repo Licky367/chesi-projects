49b1c7af5598a15652ff7ec2b1cbfca2df188ae4
Licky367 <licky367@gmail.com>
2026-09-08 13:59:32 +0300

    Create category.js

---
+++ verrah/models/category.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      index: true
+    },
+
+    categoryIcon: {
+      type: String,
+      trim: true,
+      default: ""
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+      index: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model(
+  "Category",
+  categorySchema
+);
