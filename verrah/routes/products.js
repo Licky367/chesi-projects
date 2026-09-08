cbe011bb86b4a0f51a8f975105e69abb028f78f8
Licky367 <licky367@gmail.com>
2026-09-08 08:17:11 +0300

    Update products.js

--- verrah/routes/products.js
+++ verrah/routes/products.js
@@ -1,6 +1,6 @@
 // =========================================================
 // routes/products.js
-// =========================================================
+// =======================================================
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/products");
