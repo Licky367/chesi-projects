95c2932a56d6902a52645a143f936e70da622749
Licky367 <licky367@gmail.com>
2026-09-10 08:05:58 +0300

    Update omit.js

--- utils/omit.js
+++ utils/omit.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Product = require("../models/products");
+const Product = require("../verrah/models/products");
 
 
 // ==========================================================
