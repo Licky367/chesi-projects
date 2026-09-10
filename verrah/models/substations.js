6fc96b96f8323bad3dd5a38f8afeeb0403cce588
Licky367 <licky367@gmail.com>
2026-09-10 10:11:50 +0300

    Update substations.js

--- verrah/models/substations.js
+++ verrah/models/substations.js
@@ -127,7 +127,7 @@ const substationSchema = new mongoose.Schema(
     // ------------------------------------------------------
 
     phoneNumber: {
-      type: Number,
+      type: string,
       default: null
     },
 
