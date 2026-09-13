dba3e8afd9256294d45ce786e9e2fc3c77909f79
Licky367 <licky367@gmail.com>
2026-09-13 21:21:48 +0300

    Create salesService/index.js

---
+++ verrah/services/salesService/index.js
@@ -0,0 +1,20 @@
+// ==========================================================
+// verrah/services/salesService/index.js
+// SALES SERVICE EXPORTS
+// ==========================================================
+
+module.exports = {
+
+    ...require("./page"),
+
+    ...require("./filter"),
+
+    ...require("./summary"),
+
+    ...require("./staffSales"),
+
+    ...require("./products"),
+
+    ...require("./arrears")
+
+};
