eb53f4ec7d03b9c5432d5d512a1af5a6a88b043a
Licky367 <licky367@gmail.com>
2026-09-20 10:07:05 +0300

    Update index.js

--- verrah/controllers/stock/index.js
+++ verrah/controllers/stock/index.js
@@ -2,9 +2,13 @@ const {list} = require("./list");
 const {newStockForm} = require("./forms");
 const {createOrUpdateStock} = require("./create-update");
 const {entry, createProduct} = require("./allocation");
+const {batches, batch} = require("./batch");
 
 exports.list = list;
 exports.newStockForm = newStockForm;
 exports.createOrUpdateStock = createOrUpdateStock;
 exports.entry = entry;
 exports.createProduct = createProduct;
+
+exports.batches = batches;
+exports.batch = batch;
